f8154125ddd7d2ae3e69f5fa5e031413dfe57edf
Teppei Yoshida New Mac <teppei@njc.co.jp>
2025-01-02 23:53:56 +0900

    20250101

--- js/main.js
+++ js/main.js
@@ -167,3 +167,24 @@ $(document).ready(function () {
     });
   });
 });
+
+/* アドレスバー・ツールバーを除いた100vhの高さを取得 */
+function setHeight() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+setHeight();
+window.addEventListener("resize", setHeight);
+
+/* スライダーの設定 */
+const verticalSlider = new Swiper(".vertical-slider", {
+  direction: "vertical",
+  slidesPerView: 1,
+  speed: 600,
+  mousewheel: true,
+  pagination: {
+    el: ".vertical-slider__pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
